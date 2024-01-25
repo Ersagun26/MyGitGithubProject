b4cf55ee378fd9740d93f1d925a145498e84d5f2
Ersagun26 <ersagunyavuze@gmail.com>
2024-01-25 19:25:55 +0300

    Version02

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,6 +1,10 @@
 public class AtmApp {
     public static void main(String[] args) {
+
         System.out.println("Bu bir ATM Projesidir.");
+        //version01,
+        System.out.println("Java çok  kolay");
+        //versiyon2
 
 
 
